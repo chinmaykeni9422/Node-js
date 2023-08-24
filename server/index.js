a29318893e5f68855112da8fc01569a7828e796d
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-24 09:08:42 +0000

    url

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const http = require("http") ;
 const fs = require("fs") ;
+const url = require("url") ;
+
+//url
 
 
 // to create a server
 const myserver = http.createServer((req,res) => {
+    if(req.url === "/favicon.ico")return res.end() ;
     const log = `${Date.now()}: ${req.url} new request received\n` ;
+    const myurl = url.parse(req.url,true) ;
+    console.log(myurl) ;
     fs.appendFile('file.txt',log,(err,data) =>{
-        switch (req.url) {
+        switch (myurl.pathname) {
             case "/": res.end("Home") ;
                 break;
-            case "/about": res.end("i am chinmay") ;
+            case "/about": const qp = res.end("i am chinmay") ;
                 break;
             default: res.end("404 not found") ;
                 break;
