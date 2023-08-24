d2d30cbad710b3187d6e701e27ad7612e3fd12f7
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-24 09:23:57 +0000

    http method

---
+++ server/http_methos.js
@@ -0,0 +1,6 @@
+// HTTP method
+// get, post, put, patch, delete
+
+// GET -> when you want to get some data from server
+
+// POST -> when you want to send or mutate(change or update) some data into server
