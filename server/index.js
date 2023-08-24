60d3ab5e5172dcf9ef24fd683a2e40040ecf1c76
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-24 06:58:00 +0000

    creating log

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const http = require("http") ;
+const fs = require("fs") ;
 
 
 // to create a server
 const myserver = http.createServer((req,res) => {
-    console.log("New Req Rec..") ;
-    res.end("hello from server") ;
+    const log = `${Date.now()}: ${req.url} new request received\n` ;
+    fs.appendFile('file.txt',log,(err,data) =>{
+        switch (req.url) {
+            case "/": res.end("Home") ;
+                break;
+            case "/about": res.end("i am chinmay") ;
+                break;
+            default: res.end("404 not found") ;
+                break;
+        }
+    }) ;
 }) ;
 
 // to listen server
