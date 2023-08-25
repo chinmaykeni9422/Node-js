707694409561b537be59b47bf8673d31cba425c6
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-25 08:44:13 +0000

    http method

--- server/http_methos.js
+++ server/http_methos.js
@@ -4,3 +4,9 @@
 // GET -> when you want to get some data from server
 
 // POST -> when you want to send or mutate(change or update) some data into server
+
+// PUT -> The HTTP PUT request method is used to create a new resource or overwrite a representation of the target resource that is known by the client
+
+// PATCH -> The PATCH method applies  modifications to a data.
+
+// DELETE -> The DELETE method deletes the specified data.
