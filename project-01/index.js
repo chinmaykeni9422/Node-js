3c3380be3d55e4d6bfc2dd547ed7d9b998b8f567
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-29 12:45:09 +0530

    post

--- project-01/index.js
+++ project-01/index.js
@@ -18,6 +18,8 @@ app.get('/users' , (req,res) => {
     res.send(html) ;
 }) ;
 
+// REST API's 
+
 app.get('/api/users' , (req,res) => {
     //using "res.json()" cuz we are working in json 
     return res.json(users) ;
@@ -29,8 +31,23 @@ app.get('/api/users' , (req,res) => {
 // : => variable | Dynamic
 app.get('/api/users/:id', (req,res) => {
     const id = Number(req.params.id) ;
-    const user = users.find((user) => user.id === id) ;
+    const user = users.find(user =>user.id === id) ;
     return res.json(user) ;
+}) ;
+
+app.post("/api/user",(req,res) => {
+    // TODO : create server
+    return res.json({status : "pending"}) ;
+}) ;
+
+app.patch("/api/user/:id",(req,res) => {
+    // TODO : edit the user with id
+    return res.json({status : "pending"}) ;
+}) ;
+
+app.delete("/api/user/:id",(req,res) => {
+    // TODO : delete the user with id
+    return res.json({status : "pending"}) ;
 }) ;
 
 
